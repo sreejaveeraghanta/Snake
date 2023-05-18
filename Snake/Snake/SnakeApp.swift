4cc2f37a7b3d5c58553d22e52e4eded7c036b1ed
sreejaveeraghanta <sreeja.veeraghanta@outlook.com>
2023-05-18 12:55:35 -0700

    initial commit

---
+++ Snake/Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Sreeja V on 2023-05-18.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
